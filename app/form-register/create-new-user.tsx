3708a8e8018cadc7832f80e4d774f38ae3a219f8
Jakiro12 <eclipsisnocte@gmail.com>
2024-10-16 17:01:24 -0300

    fix:crea nuevos usuarios

--- app/form-register/create-new-user.tsx
+++ app/form-register/create-new-user.tsx
@@ -1,31 +1,50 @@
 import { Text, View, StatusBar, ScrollView, TextInput, TouchableOpacity } from "react-native";
 import styles from '../../styles/new-user-styles/create-user-styles';
 import React, { useState } from "react";
+import {getFirestore, collection, addDoc} from 'firebase/firestore'
+import { firebaseconn } from "@/constants/FirebaseConn";
 interface NewUserTypes{
-        realName:string,
-        lastName:string,
-        document:string,
+        nombre:string,
+        apellido:string,
+        documento:string,
         email:string,
-        phone:string,
-        department:string,
-        establishment:string
+        celular:string,
+        departamento:string,
+        establecimiento:string
 }
 export default function CreateNewUser() {
     const statusBarHeight:number | undefined  = StatusBar.currentHeight;
     const [newUser,setNewUser]=useState<NewUserTypes>({
-        realName:'',
-        lastName:'',
-        document:'',
+        nombre:'',
+        apellido:'',
+        documento:'',
         email:'',
-        phone:'',
-        department:'',
-        establishment:''
+        celular:'',
+        departamento:'',
+        establecimiento:''
     })
+    const analytics = getFirestore(firebaseconn) 
+    const dataAdd=collection(analytics,'nuevoAfiliado')
     const handleNewUserData=(key:string ,value:string )=>{
         setNewUser({...newUser,[key]:value})
       }
-    const onSubmitForm=()=>{
+    const onSubmitForm=async()=>{
         console.log(newUser)
+        const test={
+            apellido:'hola',
+            celular:"123123123",
+            cod:1231,
+            departamento:"Valle Viejo",
+            descuento:"si",
+            dni:"37282591",
+            email:"testest@gmail.com",
+            establecimientos:"",
+            fecha:"15/09/2022 21:23:01",
+            nombre:"test"
+        }
+        const testeado= await addDoc(dataAdd,test)
+         return console.log(testeado.id)
+     
     }
     return (
         <View style={{ flex: 1, paddingTop: statusBarHeight }}>
@@ -47,22 +66,22 @@ export default function CreateNewUser() {
                     <View style={styles.inputContainer}>
                         <Text style={{color:'#ffffff',alignSelf:'flex-start',fontSize:18}}>Nombre</Text>
                         <TextInput style={styles.inputForm}
-                            value={newUser.realName}
-                            onChangeText={(getValue)=>handleNewUserData('realName',getValue)}
+                            value={newUser.nombre}
+                            onChangeText={(getValue)=>handleNewUserData('nombre',getValue)}
                         />
                     </View>
                     <View style={styles.inputContainer}>
                         <Text style={{color:'#ffffff',alignSelf:'flex-start',fontSize:18}}>Apellido</Text>
                         <TextInput style={styles.inputForm}
-                             value={newUser.lastName}
-                             onChangeText={(getValue)=>handleNewUserData('lastName',getValue)}
+                             value={newUser.apellido}
+                             onChangeText={(getValue)=>handleNewUserData('apellido',getValue)}
                         />
                     </View>
                     <View style={styles.inputContainer}>
                         <Text style={{color:'#ffffff',alignSelf:'flex-start',fontSize:18}}>DNI</Text>
                         <TextInput style={styles.inputForm} 
-                            value={newUser.document}
-                            onChangeText={(getValue)=>handleNewUserData('document',getValue)}
+                            value={newUser.documento}
+                            onChangeText={(getValue)=>handleNewUserData('documento',getValue)}
                         />
                     </View>
                     <View style={styles.inputContainer}>
@@ -75,22 +94,22 @@ export default function CreateNewUser() {
                     <View style={styles.inputContainer}>
                         <Text style={{color:'#ffffff',alignSelf:'flex-start',fontSize:18}}>Celular</Text>
                         <TextInput style={styles.inputForm} 
-                             value={newUser.phone}
-                             onChangeText={(getValue)=>handleNewUserData('phone',getValue)}
+                             value={newUser.celular}
+                             onChangeText={(getValue)=>handleNewUserData('celular',getValue)}
                         />
                     </View>
                     <View style={styles.inputContainer}>
                         <Text style={{color:'#ffffff',alignSelf:'flex-start',fontSize:18}}>Departamento</Text>
                         <TextInput style={styles.inputForm} 
-                            value={newUser.department}
-                            onChangeText={(getValue)=>handleNewUserData('department',getValue)}
+                            value={newUser.departamento}
+                            onChangeText={(getValue)=>handleNewUserData('departamento',getValue)}
                         />
                     </View>
                     <View style={styles.inputContainer}>
                         <Text style={{color:'#ffffff',alignSelf:'flex-start',fontSize:18}}>Establecimientos</Text>
                         <TextInput style={styles.inputForm} 
-                             value={newUser.establishment}
-                             onChangeText={(getValue)=>handleNewUserData('establishment',getValue)}
+                             value={newUser.establecimiento}
+                             onChangeText={(getValue)=>handleNewUserData('establecimiento',getValue)}
                         />
                     </View>
                     <TouchableOpacity 
