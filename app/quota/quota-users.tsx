304a4a1e72aa91aa09992ad094f27f729994362a
Jakiro12 <eclipsisnocte@gmail.com>
2024-11-05 11:58:50 -0300

    feat:copiar texto del alias funcionando

--- app/quota/quota-users.tsx
+++ app/quota/quota-users.tsx
@@ -1,19 +1,51 @@
-import { StatusBar, Text, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, StatusBar, Text, TouchableOpacity, View, Image } from "react-native";
 import styles from "../../styles/quota/quota-styles";
+import { firebaseconn } from "@/constants/FirebaseConn";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import * as Clipboard from 'expo-clipboard';
+
 export default function FileForQuotaPayment() {
   const statusBarHeight = StatusBar.currentHeight;
-//Los datos para transferencia y numero de telefono de afiliado aderente
-// Caja amarilla 
+  const [quotaData,setQuotaData]=useState<any>([])
+  const [loading,setLoading]=useState<boolean>(false)
+  const analytics = getFirestore(firebaseconn) 
+  const data=collection(analytics,'cuotas')
+  useEffect(()=>{
+    const getData=async()=>{
+        try {
+          setLoading(true)
+            const res=(await getDocs(data)).docs
+            setQuotaData(res[0].data())
+        } catch (error) {
+            alert(`Error:${error}`)
+        }finally {
+            setLoading(false); 
+        }
+    }
+    getData()
+    },[])
+    const copyToClipboard = async () => {
+      await Clipboard.setStringAsync(quotaData.alias);
+    };
   return (
     <View style={{ height: "100%", paddingTop: statusBarHeight }}>
-      <View style={styles.container}>
-        <View style={styles.quotaBox}>
-        <TouchableOpacity style={styles.btnGetAlias}>
-          <Text style={{fontSize:18,color:'#ffffff',fontWeight:'bold'}}>
-            Copiar Alias
-          </Text>
-        </TouchableOpacity>
-        </View>
+      <View style={styles.container}>       
+        {loading ? ( 
+                    <ActivityIndicator size="large" color="#ffffff" />
+                ) : (   
+                  <View style={styles.quotaBox}>
+                  <Image src={quotaData.imagen} style={{width:'95%',height:'80%',borderWidth:1,borderColor:'#ff0000'}} resizeMode="contain"/>
+                  <Text style={{width:'95%',height:'auto'}}>{quotaData.descripcion}</Text>
+                    <TouchableOpacity style={styles.btnGetAlias}
+                      onPress={copyToClipboard}
+                      >
+                      <Text style={{fontSize:18,color:'#ffffff',fontWeight:'bold'}}>
+                        Copiar Alias
+                      </Text>
+                    </TouchableOpacity>
+                  </View>                                         
+                )}
       </View>
     </View>
   );
