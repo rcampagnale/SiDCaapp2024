88fb2436bf7599485082ea14e2c513bbf4ef0cc9
rcampagnale <162648456+rcampagnale@users.noreply.github.com>
2025-01-02 19:24:10 -0300

    se elimina el sub menu y sus estilos

--- app/home/log-out.tsx
+++ app/home/log-out.tsx
@@ -1,38 +1,20 @@
-import { Text, TouchableOpacity, ScrollView, View } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import { router } from "expo-router";
-
 export default function CloseApp() {
   return (
-    <ScrollView
-      contentContainerStyle={{
-        flexGrow: 1, // Esto hace que el ScrollView ocupe todo el espacio disponible
-        justifyContent: "flex-end", // Coloca el contenido al final del ScrollView
-        paddingBottom: 20, // Agrega un poco de espacio en la parte inferior
+    <TouchableOpacity
+      style={{
+        width: "auto",
+        height: "auto",
+        marginRight: 20,
+        paddingTop: 3,
+        paddingBottom: 3,
+        paddingHorizontal: 10,
       }}
+      activeOpacity={1}
+      onPress={() => router.navigate("/")}
     >
-      <View
-        style={{
-          width: "auto",
-          height: 40, // Establece una altura fija para el botón
-          backgroundColor: "#007BFF", // Fondo azul
-          justifyContent: "center", // Centra el texto verticalmente
-          alignItems: "center", // Centra el texto horizontalmente
-          marginRight: 0,
-          paddingTop: 3,
-          paddingBottom: 3,
-          paddingHorizontal: 10,
-          borderRadius: 5, // Bordes redondeados para mejorar la apariencia
-        }}
-      >
-        <TouchableOpacity
-          activeOpacity={1}
-          onPress={() => router.navigate("/")}
-        >
-          <Text style={{ color: "white", fontWeight: "bold" }}>
-            Cerrar Sección
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </ScrollView>
+      <Text>Salir</Text>
+    </TouchableOpacity>
   );
 }
